e133ed4c10ceecdcbebda7d6ffebf5fbf6f64397
yankovz <43785723+yankovz@users.noreply.github.com>
2018-11-20 12:27:46 +0200

    scripts

---
+++ scripts/customers_insert.js
@@ -0,0 +1,9 @@
+db.getCollection("customers").insert([{name:"PWC Law Division (4331)",Case: "Foxcon Due Diligence (4331/021)",phone:"035249441"
+},{name:"PWC Law Division (4331)", Case: "Foxcon Due Diligence (4331/021)",phone:"0507551111"
+},{name:"PWC Law Division (4331)", Case: "Foxcon Due Diligence (4331/021)",phone:"0549849428"
+},{name:"PWC Law Division (4331)", Case: "Smith vs IRS (4331/82)",phone:"0545597024"
+},{name:"PWC Real Estate Division (4337)", Case: "Wembley Building (4337/991)",phone:"05544222"
+}])
+
+
+
